d977d29cdd95d993b135c0d6d471d5dd934d6999
Prahladkumar11 <Prahladkuma11@gmail.com>
2024-06-27 18:32:03 +0530

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
+python -m venv venv
 
 source venv/Scripts/activate
 # Install dependencies
+
 pip install --upgrade pip
 pip install -r requirements.txt
 
